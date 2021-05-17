f1fc98b767d58a9e2ec1bc0c150e5b0adc10e39d
Henri Bode <hcbode6@gmail.com>
2021-05-17 17:08:12 +0200

    neu17:08

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,18 @@ const routes: Routes = [
     path: 'brillen-uebersicht',
     loadChildren: () => import('./pages/brillen-uebersicht/brillen-uebersicht.module').then(m => m.BrillenUebersichtPageModule)
   },
+  {
+    path: 'kontaktlinsen',
+    loadChildren: () => import('./pages/kontaktlinsen/kontaktlinsen.module').then(m => m.KontaktlinsenPageModule)
+  },
+  {
+    path: 'meine-favoriten',
+    loadChildren: () => import('./pages/meine-favoriten/meine-favoriten.module').then(m => m.MeineFavoritenPageModule)
+  },
+  {
+    path: 'fehlerseite',
+    loadChildren: () => import('./pages/fehlerseite/fehlerseite.module').then(m => m.FehlerseitePageModule)
+  },
 ];
 @NgModule({
   imports: [
