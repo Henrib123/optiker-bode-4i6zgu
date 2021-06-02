06e2d7880089119984d1c2356fe569f407a6747b
Henri Bode <hcbode6@gmail.com>
2021-06-02 16:15:23 +0200

    today

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -22,184 +22,194 @@ export class DataService {
       field_sun_material_txt: "metall",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 3",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      },
+      {
       id: "2",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 4",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      },
+      {
       id: "3",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 5",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      }, {
       id: "4",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 6",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      }, {
       id: "5",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 8",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      }, {
       id: "6",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille 7",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      }, {
       id: "7",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      }, {
       id: "8",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"
+      },
+      {
       id: "9",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "10",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+      {
       id: "11",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+       {
       id: "12",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+      {
       id: "13",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "14",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    {
       id: "15",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    {
       id: "16",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    {
       id: "17",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    {
       id: "18",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
 
-    }, {
+     {
       id: "19",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "20",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "21",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "22",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "23",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "24",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "25",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    {
       id: "26",
       field_sun_item: "15674",
       field_sun_material_txt: "acetat",
       field_sun_shape_txt: "cat eye",
       title: "DEMO Sonnebrille",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }, {
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+     {
       id: "27",
       field_sun_item: "15674",
       field_sun_material_txt: "kunststoff",
       field_sun_shape_txt: "oval",
       title: "DEMO Sonnebrille 1",
-        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    }, {
-    }]
+        vue_listing_image: "https://www.stage-y77w3ti-5tykmmpq47ouw.eu-4.platformsh.site/sites/default/files/images/e744be01deb96f349ea7c0eae3d75c17/576/0AR_112MJ__3247_030A.webp"    },
+    ]
   }
 }
 
